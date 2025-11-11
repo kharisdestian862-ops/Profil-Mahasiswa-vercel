256f234945c1f0ea96cc97cae31e9db64e82594f
RiRay <kharisdestian862@gmail.com>
2025-11-11 23:41:09 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -38,7 +38,7 @@ export default async function handler(request, response) {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          model: "llama3-8b-8192",
+          model: "llama-3.1-8b-instant",
           messages: [
             {
               role: "system",
